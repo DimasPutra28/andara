fb9e04e807d7627120696505003675247ed7c002
DimasPutra28 <dimas.meidiansyah.putra-2021@vokasi.unair.ac.id>
2023-06-05 14:23:38 +0700

    terakhir 05/06/2023: 14.23

--- resources/views/pilihpasien.blade.php
+++ resources/views/pilihpasien.blade.php
@@ -1,5 +1,86 @@
 @extends('layouts.main1')
 @section('bayu')
+    <div class="direktori" style="padding: 10px">
+        <a href="" style="position: absolute; top: 100px; left: 50px; text-decoration: none">Home</a>
+        <a style="position: absolute; top: 100px; left: 105px"> / </a>
+        <a href="" style="position: absolute; top: 100px; left: 125px; text-decoration: none">Profile</a>
+    </div>
+
+    <section id="register" class="register" style="margin: 150px 0 150px 0">
+        <h1 class="text-center mb-5" style="color: #AAC4FF; font-weight: bold">Pendaftaran Pemeriksaan</h1>
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-5">
+
+                <div class="card" style="border-radius: 5px;">
+                    <div class="card-header" style="background-color: #AAC4FF; color: white">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <h4 style="color: white">Nama Lengkap</h4>
+                            </div>
+                            <div class="col-lg-7">
+                                <h4>Sophianne Azzahra</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <h4>NIK</h4>
+                                <h4>Tanggal lahir</h4>
+                                <h4>Alamat</h4>
+                            </div>
+                            <div class="col-lg-7">
+                                <h4>3578166806030002</h4>
+                                <h4>2003-06-28</h4>
+                                <h4>Jl. Kenangan No.57 Surabaya</h4>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
 
 
+            </div>
+            <div class="col-lg-5">
+
+                <div class="card" style="border-radius: 5px;">
+                    <div class="card-header" style="background-color: #AAC4FF; color: white">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <h4 style="color: white">Nama Lengkap</h4>
+                            </div>
+                            <div class="col-lg-7">
+                                <h4>Sophianne Azzahra</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <h4>NIK</h4>
+                                <h4>Tanggal lahir</h4>
+                                <h4>Alamat</h4>
+                            </div>
+                            <div class="col-lg-7">
+                                <h4>3578166806030002</h4>
+                                <h4>2003-06-28</h4>
+                                <h4>Jl. Kenangan No.57 Surabaya</h4>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="hayo text-end mt-5">
+                    <button class="btn btn-light btn-lg text-white"
+                        style="background-color: #B1B2FF; padding: 7px 15px 7px 15px" type="submit">LANJUT</button>
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+
+    </section>
 @endsection
--- resources/views/riwayat.blade.php
+++ resources/views/riwayat.blade.php
@@ -1,36 +1,200 @@
 @extends('layouts.main1')
 @section('bayu')
+    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 
-<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css"/>
+    <div class="direktori" style="padding: 10px">
+        <a href="" style="position: absolute; top: 100px; left: 50px; text-decoration: none">Home</a>
+        <a style="position: absolute; top: 100px; left: 105px"> / </a>
+        <a href="" style="position: absolute; top: 100px; left: 125px; text-decoration: none">Profile</a>
+        <a style="position: absolute; top: 100px; left: 180px"> / </a>
+        <a href="" style="position: absolute; top: 100px; left: 195px; text-decoration: none">Riwayat Medis</a>
+    </div>
 
-<section id="register" class="register" style="margin: 150px 0 150px 0">
-    <h1 class="text-center mb-5" style="color: #AAC4FF; font-weight: bold">Pendaftaran Pasien Baru</h1>
-    <div class="d-flex justify-content-center">
-        <div class="col-lg-10">
-            <form action="">
-                @csrf
-                <div class="card" style="border-radius: 5px;">
-                    <div class="card-body my-5 ">
+    <section id="register" class="register" style="margin: 150px 0 150px 0">
+        {{-- <h1 class="text-center mb-5" style="color: #AAC4FF; font-weight: bold">Pendaftaran Pasien Baru</h1> --}}
+        <div class="d-flex justify-content-center">
+            <div class="col-lg-10">
+
+                <div class="card text-center" style="border-radius: 5px;">
+                    <div class="card-header" style="background-color: #AAC4FF; color: white">
+                        <div class="row">
+                            <div class="col-lg-2">
+                                <h4>Tanggal</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Keluhan</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tindakan</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>Dokter Spesialis</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body my-3 " style="color: #676667">
+                        <div class="row" style="background-color: #AAC4FF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #D2DAFF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #AAC4FF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #D2DAFF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #AAC4FF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #D2DAFF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #AAC4FF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #D2DAFF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+                        <div class="row" style="background-color: #AAC4FF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
+                        <div class="row" style="background-color: #D2DAFF">
+                            <div class="col-lg-2">
+                                <h4>04/05/2023	</h4>
+                            </div>
+                            <div class="col-lg-3">
+                                <h4>Gigi bolong	</h4>
+                            </div>
+                            <div class="col-lg-2">
+                                <h4>Tambal gigi	</h4>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>drg. Amelia Yunisa, MKes, SpOrt</h4>
+                            </div>
+                        </div>
+
 
                     </div>
                 </div>
-                <div class="hayo text-end mt-5">
-                    <button class="btn btn-light btn-lg text-white" style="background-color: #B1B2FF; padding: 7px 15px 7px 15px" type="submit">CONFIRM</button>
-                </div>
-            </form>
+                {{-- <div class="hayo text-end mt-5">
+                        <button class="btn btn-light btn-lg text-white"
+                            style="background-color: #B1B2FF; padding: 7px 15px 7px 15px" type="submit">CONFIRM</button>
+                    </div> --}}
+                </form>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
 
-<script src="assets/plugins/global/plugins.bundle.js"></script>
-<script>
-    $(document).off('.datepicker.data-api');
-    $('.datepicker').datepicker({
-    format: 'mm/dd/yyyy',
-    startDate: '-3d'
-});
-
-</script>
-
+    <script src="assets/plugins/global/plugins.bundle.js"></script>
 @endsection
+
